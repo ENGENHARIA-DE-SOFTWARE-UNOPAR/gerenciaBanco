f7480d3f1050168f7e71c1fc2fb82255b628810d
OgliariNatan <natanogliari@gmail.com>
2023-09-22 11:06:57 -0300

    add loop

--- gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
+++ gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
@@ -1,6 +1,11 @@
 
 package gerenciabanco;
 
+import java.util.*;
+//import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
+import java.lang.Integer;
 /**
  *
  * @author Natan Ogliari
@@ -25,7 +30,22 @@ public class GerenciaBanco {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        while(true){
+            // Scanner menu = new Scanner(System.in);// habilita a leitura pelo teclado
+            JOptionPane.showMessageDialog(null,"Bem vindo ao banco Saracura do Banhado\n");
+            String opcao = JOptionPane.showInputDialog(null,"Opção 1 - Consulta saldo\n Opção 2 - Realizar um deposito\n Opção 3 - Realizar um saque\n Opção 4 - Sair \n");
+        
+              //conversão de String para int
+            int control = Integer.parseInt(opcao);
+        
+              //controle de erro
+            if ("opcao"  == null){//caso o usuario cancele a opção
+                JOptionPane.Dialog(null, "Você cancelou a operação");
+                break;            
+            }        
+        }
+               
     }
     
 }
