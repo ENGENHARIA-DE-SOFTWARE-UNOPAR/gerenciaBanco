c2108d67f881d5c8b3ea3f2528a66fedb179b0f5
OgliariNatan <natanogliari@gmail.com>
2023-09-28 11:26:49 -0300

    info

--- gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
+++ gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
@@ -18,11 +18,11 @@ public class GerenciaBanco {
         String sobrenome;
         String cpf;
         String conta;
+        String saldo;
         
         
         public void Conta(){
             int numConta;
-            String titularConta;
             double saldo;
         }    
         public void depositaSaldo(double pilas, String titularConta){
@@ -49,26 +49,37 @@ public class GerenciaBanco {
         while(true){            
             String opcao = JOptionPane.showInputDialog(null,"Opção 1 - Consulta saldo\n Opção 2 - Realizar um deposito\n Opção 3 - Realizar um saque\n Opção 4 - Sair \n",4);//deixa a opçã4 4 como deful
             //conversão de String para int
+            
+            //Object[] itens = {"Saque","Consulta Saldo","Realiza Saque","Sair"};//!<Para melhorias
+            //Object valorSelecionado = JOptionPane.showInputDialog(null,"Escolha um item", "Opçao",JOptionPane.INFORMATION_MESSAGE, null,itens, itens [0]);
+            
             int control = Integer.parseInt(opcao);
         
               //controle de erro
             if ("opcao"  == null){//caso o usuario cancele a opção
-                //JOptionPane.showMessageDialog(null, "Você cancelou a operação");
+                JOptionPane.showMessageDialog(null, "Você cancelou a operação");
                 break;            
             }
             if (control == 4){//para sair da operação
-                JOptionPane.showMessageDialog(null, "Volte sempre "+cliente.nome +cliente.sobrenome+"\nTenha um bom dia!.");
+                JOptionPane.showMessageDialog(null,"Volte sempre "+cliente.nome+" "+cliente.sobrenome+"\nTenha um bom dia!", "LOGOUT", JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
                 break;
             }
             switch (control){
                 case 1:
-                    JOptionPane.showMessageDialog(null, "Consulta saldo.");
+                    JOptionPane.showMessageDialog(null,"Consulta saldo.", cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
+                    
                     break;
                 case 2:
-                    JOptionPane.showMessageDialog(null, "Realizar um deposito.");
+                    JOptionPane.showMessageDialog(null,"Realizar um deposito.", cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                    int pilaDeposito = Integer.parseInt(JOptionPane.showInputDialog(null,"Informa a quantidade em Reais (R$) a ser depositado na conta do "+cliente.nome+" "+cliente.sobrenome));
+                    
+                    
+                    JOptionPane.showMessageDialog(null,"Seu Saldo é ", cliente.saldo, JOptionPane.INFORMATION_MESSAGE, icon);
+                    
                     break;
                 case 3:
-                    JOptionPane.showMessageDialog(null,"Realiza um saque.");
+                    JOptionPane.showMessageDialog(null,"Realiza um saque.", cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                    
                     break;
                 default:
                     JOptionPane.showMessageDialog(null,"Opção invalida.");
