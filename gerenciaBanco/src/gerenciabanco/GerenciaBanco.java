bcd01034f85a2b0a4a24deb38c19f10308ea549c
OgliariNatan <natanogliari@gmail.com>
2023-10-03 10:02:04 -0300

    add javaDoc

--- gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
+++ gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
@@ -11,6 +11,8 @@ import java.lang.Error;
 /**
  *
  * @author Natan Ogliari
+ * 
+ * @version 0.1
  */
 public class GerenciaBanco {
     
@@ -20,20 +22,41 @@ public class GerenciaBanco {
         String sobrenome;
         String cpf;
         //String conta;
-        String saldo;
+        double saldo;
         ImageIcon icon = new ImageIcon("C:\\Users\\AULA-1\\Documents\\(Engenharia de Software)\\Fase 3\\Linguagem Orientada a Objetos\\gerenciaBanco\\gerenciaBanco\\src\\gerenciabanco\\saracura.jpg");
           
          /**
-        * @param args Pilas a ser depositado & nome da conta
+        * @param nome da conta a ser depositado
+        * @param pilas valor a ser depositado 
+        * @return 1 se deu certo e 0 se ocorreu um erro
         */
-        public void depositaSaldo(int pilas, String titularConta){
+        public int depositaPila(double pilas, String nome){
             
             //this.saldo += pilas;
             //JOptionPane.showMessageDialog(null,"Seu Saldo é ", this.saldo, JOptionPane.INFORMATION_MESSAGE, this.icon);
-                   
+            return 1;   
         }
-        public void saquePila(double pilas, String nome){
         
+         /**
+        * @param nome da conta a ser saquada
+        * @param pilas valor a ser saquado 
+        * @return 1 se deu certo e 0 se ocorreu um erro
+        */
+        public int saquePila(double pilas, String nome){
+        
+            
+            return 1;
+        }
+        
+         /**
+        * @param nome da conta a ser consultado
+        *
+        * @return O saldo da conta
+        */
+        public double consultaPilas(String nome){
+            pilas = this.saldo;
+        
+        return pilas;
         }
     
     }
@@ -45,20 +68,20 @@ public class GerenciaBanco {
        
         ImageIcon icon = new ImageIcon("C:\\Users\\AULA-1\\Documents\\(Engenharia de Software)\\Fase 3\\Linguagem Orientada a Objetos\\gerenciaBanco\\gerenciaBanco\\src\\gerenciabanco\\saracura.jpg");
         
-        cliente cliente = new cliente();
+        cliente cliente1 = new cliente();//instancia a cliente
         JOptionPane.showMessageDialog(null,"Bem vindo ao Banco Saracura do Banhado\n", "INÍCIO", JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
 
         try {
-            cliente.nome = JOptionPane.showInputDialog(null, "Informe seu Nome.", "Nome");
+            cliente1.nome = JOptionPane.showInputDialog(null, "Informe seu Nome.", "Nome");
             
         }
         catch (NullPointerException e){
             JOptionPane.showMessageDialog(null, "Entre com um nome válido.");
         }
         
-        cliente.sobrenome = JOptionPane.showInputDialog(null, "Informe seu Sobrenome.","Sobrenome");
-        cliente.cpf = JOptionPane.showInputDialog(null, "Informe o numéro do CPF.","000.000.000-00");
-        cliente.saldo = "445";//remover
+        cliente1.sobrenome = JOptionPane.showInputDialog(null, "Informe seu Sobrenome.","Sobrenome");
+        cliente1.cpf = JOptionPane.showInputDialog(null, "Informe o numéro do CPF.","000.000.000-00");
+        cliente1.saldo = 445;//remover
         while(true){ //InterruptedIOException
             
             //try{
@@ -77,23 +100,23 @@ public class GerenciaBanco {
                 break;            
             }
             if (control == 4){//para sair da operação
-                JOptionPane.showMessageDialog(null,"Volte sempre "+cliente.nome+" "+cliente.sobrenome+"\nTenha um bom dia!", "LOGOUT", JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
+                JOptionPane.showMessageDialog(null,"Volte sempre "+cliente1.nome+" "+cliente1.sobrenome+"\nTenha um bom dia!", "LOGOUT", JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
                 break;
             }
             switch (control){
                 case 1://consulta de saldo
-                    JOptionPane.showMessageDialog(null,"Consulta saldo.", cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
-                    JOptionPane.showMessageDialog(null,"Seu saldo atual é de: R$" +cliente.saldo, cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                    JOptionPane.showMessageDialog(null,"Consulta saldo.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
+                    JOptionPane.showMessageDialog(null,"Seu saldo atual é de: R$" +cliente1.saldo, cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
                     break;
                 case 2://realiza deposito
-                    JOptionPane.showMessageDialog(null,"Realizar um deposito.", cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);                
+                    JOptionPane.showMessageDialog(null,"Realizar um deposito.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);                
                     
                     try {// verifica se a entrada e do tipo numeral
-                        int pilaDeposito = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe a quantidade em Reais (R$) a ser depositado na conta do "+cliente.nome+" "+cliente.sobrenome));
+                        int pilaDeposito = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe a quantidade em Reais (R$) a ser depositado na conta do "+cliente1.nome+" "+cliente1.sobrenome));
                         
-                        pilaDeposito +=(Integer.parseInt(cliente.saldo));//converte para inteiro
-                        cliente.saldo =  Integer.toString(pilaDeposito);//converte para string
-                        JOptionPane.showMessageDialog(null,"Seu Saldo é R$:"+cliente.saldo+" Reais", cliente.nome, JOptionPane.INFORMATION_MESSAGE, icon);
+                        pilaDeposito +=(Integer.parseInt(cliente1.saldo));//converte para inteiro
+                        cliente1.saldo =  Integer.toString(pilaDeposito);//converte para string
+                        JOptionPane.showMessageDialog(null,"Seu Saldo é R$:"+cliente1.saldo+" Reais", cliente1.nome, JOptionPane.INFORMATION_MESSAGE, icon);
                     }
                     
                     catch (NumberFormatException e) {// imprime o erro na tela e informa o que foi digitado.
@@ -102,17 +125,17 @@ public class GerenciaBanco {
                     
                     break;
                 case 3:// realiza saque
-                    JOptionPane.showMessageDialog(null,"Realiza um saque.", cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                    JOptionPane.showMessageDialog(null,"Realiza um saque.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
                     try {// tratamento de exception
-                        int pilaSaque = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe a quantidade em Reais (R$) a ser sacada na conta do "+cliente.nome+" "+cliente.sobrenome));
-                        int saldoAtual = Integer.parseInt(cliente.saldo);//Variavel TEMP com conversão
+                        int pilaSaque = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe a quantidade em Reais (R$) a ser sacada na conta do "+cliente1.nome+" "+cliente1.sobrenome));
+                        int saldoAtual = Integer.parseInt(cliente1.saldo);//Variavel TEMP com conversão
                         
                         if (saldoAtual >= pilaSaque){//verifica se tem saldo
                             saldoAtual -= pilaSaque; 
-                            cliente.saldo = Integer.toString(saldoAtual);//converte para String              
+                            cliente1.saldo = Integer.toString(saldoAtual);//converte para String              
                         }
                         else{
-                            JOptionPane.showMessageDialog(null,"O valor informado para o saque é maior que seu saldo.\n seu saldo é de: R$ "+cliente.saldo, "Saldo Insuficiente", JOptionPane.ERROR_MESSAGE, icon);
+                            JOptionPane.showMessageDialog(null,"O valor informado para o saque é maior que seu saldo.\n seu saldo é de: R$ "+cliente1.saldo, "Saldo Insuficiente", JOptionPane.ERROR_MESSAGE, icon);
                         }
                         
                         //JOptionPane.showMessageDialog(null,"valor total: R$"+cliente.saldo+ " Reais", cliente.nome, JOptionPane.INFORMATION_MESSAGE, icon);
@@ -123,7 +146,7 @@ public class GerenciaBanco {
                     }
                     break;
                 default:
-                    JOptionPane.showMessageDialog(null,"Opção inválida.", cliente.nome+" "+cliente.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                    JOptionPane.showMessageDialog(null,"Opção inválida.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
                     
                     break;
             }
