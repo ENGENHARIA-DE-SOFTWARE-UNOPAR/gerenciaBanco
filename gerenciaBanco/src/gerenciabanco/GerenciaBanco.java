3bc409b66a7f45b1cbb4af1f4f0c4ea62b630084
OgliariNatan <natanogliari@gmail.com>
2023-10-06 09:48:17 -0300

    add controle de retorno das entidades e geralçao do javadoc

--- gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
+++ gerenciaBanco/src/gerenciabanco/GerenciaBanco.java
@@ -1,4 +1,3 @@
-
 package gerenciabanco;
 
 import java.util.*;
@@ -10,14 +9,11 @@ import java.lang.Exception;
 import java.lang.Error;
 import java.lang.reflect.Method;
 /**
- *
  * @author Natan Ogliari
- * 
  * @version 0.1
  */
 public class GerenciaBanco {
     
-        
     public static class cliente{
         String nome;
         String sobrenome;
@@ -27,22 +23,18 @@ public class GerenciaBanco {
         private static final int STATUS_OK = 1;//!<define um retorno caso de sucesso> 
         private static final int STATUS_FAIL = 0;//!<define um retorno caso de falha>
         
-        
         ImageIcon icon = new ImageIcon("C:\\Users\\AULA-1\\Documents\\(Engenharia de Software)\\Fase 3\\Linguagem Orientada a Objetos\\gerenciaBanco\\gerenciaBanco\\src\\gerenciabanco\\saracura.jpg");
           
-         /**
+        /**
         * @param nome da conta a ser depositado
-        * @return 1 se deu certo e 0 se ocorreu um erro
+        * @return retorna STATUS_OK se a operação ocorreu com sucesso e retorna STATUS_FAIL se ocorrer um erro
         */
         public int depositaPila(String nome){
-            
-                
-            
-            
+             
             try {// verifica se a entrada e do tipo numeral
                double pilaDeposito = Double.parseDouble(JOptionPane.showInputDialog(null,"Informe a quantidade em Reais (R$) a ser depositado na conta do "+this.nome+" "+this.sobrenome));
                  this.saldo += pilaDeposito;                                     
-                JOptionPane.showMessageDialog(null,"Seu Saldo é R$:"+this.consultaPilas(this.nome)+" Reais", this.nome, JOptionPane.INFORMATION_MESSAGE, icon);
+                //JOptionPane.showMessageDialog(null,"Seu Saldo é R$:"+this.consultaPilas(this.nome)+" Reais", this.nome, JOptionPane.INFORMATION_MESSAGE, icon);
                 return STATUS_OK;
             }
                     
@@ -50,39 +42,39 @@ public class GerenciaBanco {
                 JOptionPane.showMessageDialog(null,"Entre com valor válido, do tipo númeral.\n Use (.) ponto em vez de (,) virgula\n ERRO: " + e.getMessage()  , "ERRO", JOptionPane.ERROR_MESSAGE);
                 return STATUS_FAIL;
             }
-
         }
         
-         /**
-        * @param nome da conta a ser saquada
-        * @param pilas valor a ser saquado 
-        * @return 1 se deu certo e 0 se ocorreu um erro
+        /**
+        * @param nome da conta a ser saquada 
+        * @return retorna STATUS_OK se a operação ocorreu com sucesso e retorna STATUS_FAIL se ocorrer um erro
         */
-        public int saquePila(double pilas, String nome){
+        public int saquePila(String nome){
             
-            if (true){
-            
-                return STATUS_OK;
-            }
-            else{
-                return STATUS_FAIL;
+            try {// tratamento de exception
+                    double pilaSaque = Double.parseDouble(JOptionPane.showInputDialog(null,"Informe a quantidade em Reais (R$) a ser sacada na conta do "+this.nome+" "+this.sobrenome));  
+                    if (this.saldo >= pilaSaque){//verifica se tem saldo
+                        this.saldo -= pilaSaque; 
+                        return STATUS_OK;
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null,"O valor informado para o saque é maior que seu saldo.\nSeu saldo é de: R$ "+this.saldo, "Saldo Insuficiente", JOptionPane.ERROR_MESSAGE, icon);
+                        return STATUS_FAIL;
+                    }   
+            }   
+            catch (NumberFormatException e){
+                    JOptionPane.showMessageDialog(null,"Entre com valor válido, do tipo númeral.\n ERRO: " + e.getMessage()  , "ERRO", JOptionPane.ERROR_MESSAGE);
+            return STATUS_FAIL;
             }
         }
         
-         /**
-        * @param nome Passa o nome da conta a ser consultada
-        * @return Retorna o saldo da conta
+        /**
+        * @param nome Passa o nome da conta a ser consultada e informa o saldo ao cliente.
         */
-        public double consultaPilas(String nome){
-            
-            return this.saldo;
+        public void consultaPilas(String nome){
+            JOptionPane.showMessageDialog(null,"Seu saldo atual é de: R$" +this.saldo, this.nome+" "+this.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
         }
-    
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        
         ImageIcon icon = new ImageIcon("C:\\Users\\AULA-1\\Documents\\(Engenharia de Software)\\Fase 3\\Linguagem Orientada a Objetos\\gerenciaBanco\\gerenciaBanco\\src\\gerenciabanco\\saracura.jpg");
@@ -100,63 +92,52 @@ public class GerenciaBanco {
         
         cliente1.sobrenome = JOptionPane.showInputDialog(null, "Informe seu Sobrenome.","Sobrenome");
         cliente1.cpf = JOptionPane.showInputDialog(null, "Informe o numéro do CPF.","000.000.000-00");
-        cliente1.saldo = 445;//remover
+        //cliente1.saldo = 445;//remover
         while(true){ //InterruptedIOException
             
-            //try{
             String opcao = JOptionPane.showInputDialog(null,"Opção 1 - Consulta saldo\n Opção 2 - Realizar um deposito\n Opção 3 - Realizar um saque\n Opção 4 - Sair \n",4);//deixa a opçã4 4 como deful
             //conversão de String para int
             int control = Integer.parseInt(opcao);
-                  
-                              
+                           
             if ("opcao"  == null){//caso o usuario cancele a opção
                 JOptionPane.showMessageDialog(null, "Você cancelou a operação");
                 break;            
             }
+            
             if (control == 4){//para sair da operação
                 JOptionPane.showMessageDialog(null,"Volte sempre "+cliente1.nome+" "+cliente1.sobrenome+"\nTenha um bom dia!", "LOGOUT", JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
                 break;
             }
+            
             switch (control){
-                case 1://consulta de saldo
-                    //double temp = cliente1.consultaPilas(cliente1.nome);
+                case 1://consulta de saldo   consultaPilas(nome) 
                     JOptionPane.showMessageDialog(null,"Consulta saldo.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);//add custon icon
-                    JOptionPane.showMessageDialog(null,"Seu saldo atual é de: R$" +cliente1.consultaPilas(cliente1.nome), cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                    cliente1.consultaPilas(cliente1.nome);  
                     break;
+                
                 case 2://realiza deposito
                     JOptionPane.showMessageDialog(null,"Realizar um deposito.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);                
                     int verifica = cliente1.depositaPila(cliente1.nome);
                     if (verifica == 1){
-                    
+                        JOptionPane.showMessageDialog(null,"Depósito realizado com sucesso.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
                     }else{
-                        JOptionPane.showMessageDialog(null,"Falha no deposito.\n Tente novamente.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                        JOptionPane.showMessageDialog(null,"Falha no deposito.\n Tente novamente.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.ERROR_MESSAGE, icon);
                     }
-                    
                     break;
+                
                 case 3:// realiza saque
-                    JOptionPane.showMessageDialog(null,"Realiza um saque.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
-                    try {// tratamento de exception
-                        double pilaSaque = Double.parseDouble(JOptionPane.showInputDialog(null,"Informe a quantidade em Reais (R$) a ser sacada na conta do "+cliente1.nome+" "+cliente1.sobrenome));
-                       
-                       
-                        //saquePila(pilaSaque, temp);
-                        
-                        if (cliente1.saldo >= pilaSaque){//verifica se tem saldo
-                            cliente1.saldo -= pilaSaque; 
-                                       
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"O valor informado para o saque é maior que seu saldo.\n seu saldo é de: R$ "+cliente1.saldo, "Saldo Insuficiente", JOptionPane.ERROR_MESSAGE, icon);
-                        }
-                        
-                    }   
-                    catch (NumberFormatException e){
-                        JOptionPane.showMessageDialog(null,"Entre com valor válido, do tipo númeral.\n ERRO: " + e.getMessage()  , "ERRO", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null,"Realiza um saque.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);                   
+                    verifica = cliente1.saquePila(cliente1.nome);
+                    
+                    if (verifica == 1){
+                        JOptionPane.showMessageDialog(null,"Saque realizado com sucesso.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
+                    }else{
+                        JOptionPane.showMessageDialog(null,"Falha no saque.\n Tente novamente.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.ERROR_MESSAGE, icon);
                     }
                     break;
+                
                 default:
-                    JOptionPane.showMessageDialog(null,"Opção inválida.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);
-                    
+                    JOptionPane.showMessageDialog(null,"Opção inválida.", cliente1.nome+" "+cliente1.sobrenome, JOptionPane.INFORMATION_MESSAGE, icon);  
                     break;
             }
         }
